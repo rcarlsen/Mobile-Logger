92d051cdf414440c49d1b9e4903d6fb59910b3e3
Robert Carlsen <robert@robertcarlsen.net>
2010-03-31 01:35:41 -0400

    Added/applied GPLv3 to project.
    Cleaned up extra files from directories.
    Updated icon and artwork for release.

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,24 @@
+/*
+ * Mobile Logger. Record geotagged sensor values on a mobile device.
+ * Copyright (C) 2010 Robert Carlsen
+ *
+ * This file is part of Mobile Logger.
+ *
+ * Mobile Logger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
